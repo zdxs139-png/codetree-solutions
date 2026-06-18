eb42f07f16af0273c6e80c0dcfe5f9f53b620ec8
zdxs139-png <zdxs139@gmail.com>
2026-06-18 16:27:48 +0900

    [Passed] 사칙연산 연습 (94ms, 9MB)

---
+++ 260618/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 30;
+        int c = 35;
+        System.out.println(c / 8);
+        System.out.println(c % 8);
+    }
+}
