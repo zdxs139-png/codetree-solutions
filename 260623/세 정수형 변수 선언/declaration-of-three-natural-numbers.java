67f113acec0bbf39ec46ba141924f01371fc8e94
zdxs139-png <zdxs139@gmail.com>
2026-06-23 15:46:39 +0900

    [Passed] 세 정수형 변수 선언 (89ms, 9MB)

---
+++ 260623/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 23;
+        int num3 = 30;
+
+        System.out.println(num1 + " + "+ num2 + " = " + num3);
+    }
+}
